e51b33a8570e99af7ea8518b415f6cb1116c6138
donknap <donknap@gmail.com>
2025-09-12 09:54:27 +0800

    update

---
+++ .vitepress/components/DPAds.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { DefaultTheme } from 'vitepress/theme'
+import { computed } from 'vue'
+</script>
+
+<template>
+  <div class="DPCenter">
+    <div class="container">
+      <a href="https://www.lcayun.com/actcloud.html" target="_blank"><img src="https://cdn.w7.cc/dpanel/6bc4515550b8c066261317ce5cd5ab02.png" width="100%" style="height: 80px;" /></a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.DPCenter {
+  position: relative;
+  padding: 0 24px;
+  margin-bottom: 20px;
+}
+
+@media (min-width: 640px) {
+  .DPCenter {
+    padding: 0 48px;
+  }
+}
+
+@media (min-width: 960px) {
+  .DPCenter {
+    padding: 0 64px;
+  }
+}
+
+.container {
+  margin: 0 auto;
+  max-width: 1152px;
+}
+</style>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,25 +4,14 @@ import type { Theme } from 'vitepress'
 import DefaultTheme, { VPImage } from 'vitepress/theme'
 import './style.css'
 import mediumZoom from 'medium-zoom'
+import DPAds from '../components/DPAds.vue'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-      'home-hero-after': () => h("div", {
-        style: {
-          overflow: "hidden",
-          margin: "0px auto",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          marginBottom: "30px"
-        }
-      }, h("img", {
-        src: "https://cdn.w7.cc/dpanel/dpanel-logo-1.png",
-        width: 100,
-      }))
+      'home-hero-after': () => h(DPAds)
     })
   },
   enhanceApp({ app, router, siteData }) {
